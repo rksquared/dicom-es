f3ddeb760f1cc15a0344b150b231edb8e5f0d989
rksquared <rahul@rahulkrish.com>
2018-04-18 11:48:30 -0400

    added CORS handling middleware

--- src/app.ts
+++ src/app.ts
@@ -6,18 +6,30 @@ const app = express();
 
 const port = 5005;
 
+
+//use standard CORS headers
+app.use((req, res, next) => {
+  res.header(`Access-Control-Allow-Origin`, `*`);
+  res.header(`Access-Control-Allow-Headers`, `Origin, X-Requested-With`);
+  // res.header(`Content-Type`, `Accept`);
+  next();
+});
+
 app.get('/', (req, res) => {
   console.log('inbound request @ GET "/" enpoint');
-  dcm.parse(__dirname + '../assets/dicom_291.dcm');
+  //make sure to check that the asset exists in your local copy of this repo!
+  dcm.parse(__dirname + '/../assets/000001.dcm');
   es.ping();
   res.send('recieving requests at "/"');
 });
 
 app.get('/search', (req, res) => {
+  console.log(`recieving inbound search request, searching for: ${req.query.q}`)
+
   es.search(req.query.q).then((rc) => {
-    res.send(rc);
+    res.send(JSON.stringify(rc));
   }).catch((err) => {
-    res.send('ERROR'); 
+    res.send(err); 
   });
 });
 
